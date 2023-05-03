2a77ec99a18c0f18d986ef8abaa4119ecb1fcb98
edocas01 <edoardo.castioni@gmail.com>
2023-05-03 15:31:18 +0200

    initialized repo

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration file for the DQN algorithm
+TYPE_PENDULUM = 0 # 0: simple pendulum, 1: double pendulum
+
+# Pendulum parameters
+if TYPE_PENDULUM == 0:
+
+
+else:
+    
---
+++ main.py
@@ -0,0 +1,10 @@
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+# import dependencies
+from Classes import disc_pendulum
+import config
