1153a3096e969e164eb1b648d8fcb8ea97807520
edocas01 <edoardo.castioni@gmail.com>
2023-05-03 15:50:28 +0200

    update gitignore

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # Configuration file for the DQN algorithm
 TYPE_PENDULUM = 0 # 0: simple pendulum, 1: double pendulum
 
-# Pendulum parameters
+
+# Single pendulum parameters
 if TYPE_PENDULUM == 0:
 
 
 else:
-    
+# Double pendulum parameters
--- main.py
+++ main.py
@@ -7,4 +7,5 @@ from tensorflow.keras import layers
 
 # import dependencies
 from Classes import disc_pendulum
-import config
+
+
